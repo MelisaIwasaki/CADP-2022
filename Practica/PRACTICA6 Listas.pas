9b8f5396e314b93db038829fc1e50fe15e2a3633
MERI <92184167+MelisaIwasaki@users.noreply.github.com>
2022-05-05 17:21:03 -0300

    Update PRACTICA6 Listas.pas
    Falta el 14 y 16

--- Practica/PRACTICA6 Listas.pas
+++ Practica/PRACTICA6 Listas.pas
@@ -1375,3 +1375,167 @@ begin
   recorrerVector(vec,L,vr);
   informar(L,vr);
 end.
+{
+14. La oficina de becas y subsidios desea optimizar los distintos tipos de ayuda financiera que se brinda a 
+alumnos de la UNLP. Para ello, esta oficina cuenta con un registro detallado de todos los viajes realizados por 
+una muestra de 1300 alumnos durante el mes de marzo. De cada viaje se conoce el código de alumno (entre 1 
+y 1300), día del mes, Facultad a la que pertenece y medio de transporte (1. colectivo urbano; 2. colectivo 
+interurbano; 3. tren universitario; 4. tren Roca; 5. bicicleta). Tener en cuenta que un alumno puede utilizar más 
+de un medio de transporte en un mismo día.
+Además, esta oficina cuenta con una tabla con información sobre el precio de cada tipo de viaje.
+Realizar un programa que lea la información de los viajes de los alumnos y los almacene en una estructura de 
+datos apropiada. La lectura finaliza al ingresarse el código de alumno -1, que no debe procesarse.
+Una vez finalizada la lectura, informar:
+
+a. La cantidad de alumnos que realizan más de 6 viajes por día
+b. La cantidad de alumnos que gastan en transporte más de $80 por día
+c. Los dos medios de transporte más utilizados.
+d. La cantidad de alumnos que combinan bicicleta con algún otro medio de transporte
+}
+
+
+{
+15. La cátedra de CADP está organizando la cursada para el año 2019. Para ello, dispone de una lista con todos 
+los alumnos que cursaron EPA. De cada alumno se conoce su DNI, apellido, nombre y la nota obtenida. Escribir 
+un programa que procese la información de alumnos disponible y los distribuya en turnos utilizando los 
+siguientes criterios:
+- Los alumnos que obtuvieron al menos 8 en EPA deberán ir a los turnos 1 ó 4.
+- Los alumnos que obtuvieron entre 5 y 8 deberán ir a los turnos 2, 3 ó 5.
+- Los alumnos que no alcanzaron la nota 5 no se les asignará turno en CADP.
+Al finalizar, el programa debe imprimir en pantalla la lista de alumnos para cada turno.
+Nota: La distribución de alumnos debe ser lo más equitativa posible
+}
+program cadp;
+const dimF = 5;             
+type cadena15 = string [15];
+     subrango = 1..10;
+     alumno = record
+                dni: integer;
+                nom: cadena15;
+                ape: cadena15;
+                nota: subrango;
+              end;
+     lista = ^nodo;
+     nodo = record
+                datos: alumno;
+                sig: lista;
+            end;
+     vector = array [1..dimF] of lista;
+     vectorCant = array [1..dimF] of integer;
+
+procedure leerAlu(var a:alumno);
+begin
+  writeln('Ingrese dni');
+  readln(a.dni);
+  writeln('Ingrese el nombre');
+  readln(a.nom);
+  writeln('Ingrese el apellido');
+  readln(a.ape);
+  writeln('Ingrese la nota');
+  readln(a.nota);
+end;
+procedure AgregarAdelante(var L:lista;a:alumno);
+var nue:lista;
+begin
+  new(nue);
+  nue^.datos:=a;
+  nue^.sig:=L;
+  L:=nue;
+end;
+procedure cargarLista(var L:lista); 
+var a:alumno;
+begin
+  leerAlu(a);
+  while(a.dni<>0)do begin
+    AgregarAdelante(L,a);
+    leerAlu(a);
+  end;
+end;
+procedure InicializarVector(var vc:vectorCant); 
+var i:integer;
+begin
+  for i:=1 to dimF do
+    vc[i]:=0;
+end;
+procedure InicializarListas (var v: vector);
+var i: integer;
+begin
+    For i:= 1 to dimF do
+      v[i]:= nil
+end;
+Procedure Turnos14 (var v:vector; vC: vectorCant; a: alumno);
+var num: integer;
+begin
+    if (vC[1] < vC [4]) then num:= 1 
+                        else num:= 4; 
+    AgregarAdelante (v[num], a);
+    vc[num]:= vC[num] + 1;
+end;
+function Minimo (n1,n2,n5:integer):integer;
+begin
+      if (n1 < n2) and (n1 < n5  )then  Minimo:= 1  
+      else if  (n2 < n1 )and( n2 < n5  )            
+           then Minimo:= 2
+           else Minimo:= 5;
+end;
+Procedure Turnos235 (var v:vector; vC: vectorCant; a: alumno);
+var num: integer;
+begin
+    num:= Minimo (vC[2], vC [3], vC [5]);  
+    AgregarAdelante (v[num], a);   
+    vc[num]:= vC[num] + 1;         
+end;
+procedure Distribuir (var v: vector;L:lista);
+var vC: vectorCant;
+begin
+  InicializarVector (vC);
+  while l <> nil do begin  
+    case l^.datos.nota of
+      5,6,7: Turnos235 (v,vC, l^.datos);
+      8,9,10: Turnos14 (v,vC, l^.datos);
+    else write ('No se le asigna turno')
+    end;
+  end;
+  l:= l^.sig;
+end;
+procedure ImprimirLista (l: lista);
+begin
+    while (l <> nil) do begin
+      writeln (l^.datos.nom, l^.datos.ape);
+      l:= l^.sig
+    end;
+end;
+procedure Imprimir (v: vector);
+var i: integer;
+begin
+  For i:= 1 to dimF do begin
+    writeln ('Lista del turno: ', i);
+    ImprimirLista (v[i]);
+  end;
+end;
+
+
+var  v: vector;
+     L:lista;
+begin
+  L:=nil;
+  InicializarListas (v);
+  cargarLista(L);
+  Distribuir (v,L);
+  Imprimir (v);
+end.
+{
+16. La empresa distribuidora de una app móvil para corredores ha organizado una competencia mundial, en la 
+que corredores de todos los países participantes salen a correr en el mismo momento en distintos puntos del 
+planeta. La app registra, para cada corredor, el nombre y apellido, la distancia recorrida (en kilómetros), el 
+tiempo (en minutos) durante el que corrió, el país y la ciudad desde donde partió, y la ciudad donde finalizó su 
+recorrido. Realizar un programa que permita leer y almacenar toda la información registrada durante la 
+competencia. Una vez que se han almacenado todos los datos, informar:
+o La cantidad total de corredores, la distancia total recorrida y el tiempo total de carrera de todos 
+los corredores.
+o El nombre de la ciudad que convocó la mayor cantidad de corredores y la cantidad total de 
+kilómetros recorridos por los corredores de esa ciudad.
+o La distancia promedio recorrida por corredores de Brasil.
+o La cantidad de corredores que partieron de una ciudad y finalizaron en otra ciudad.
+o El paso (cantidad de minutos por km) promedio de los corredores de la ciudad de Boston.
+}
