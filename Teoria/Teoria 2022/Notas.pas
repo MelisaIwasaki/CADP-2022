8d14e2f9ff0f816aedc85bb22b0f34f71d6bc306
MERI <92184167+MelisaIwasaki@users.noreply.github.com>
2022-05-24 13:01:20 -0300

    create :bug:

---
+++ Teoria/Teoria 2022/Notas.pas
@@ -0,0 +1,26 @@
+//Calcular un maximo en una lista
+//Precondicion: la lista esta ordenada de mayor a menor
+
+if(L<>nil) do begin
+  max:= L^.dato;
+
+//Precondicion: la lista esta ordenada de menor a mayor
+
+function maximo(L:lista):integer;
+var
+  max:integer;
+  ant:lista;
+begin
+  ant:= L;
+  while(L<>nil)do begin
+    ant:= L;
+    L:= L^.sig;
+  end;
+  if(ant<>nil)then
+    maximo:= ant^.dato
+  else
+    maximo:= -9999;
+end;
+
+
+  
