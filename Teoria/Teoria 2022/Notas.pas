b93916046045166272724d83e8b934d169d2f689
メリー緑 <92184167+MelisaIwasaki@users.noreply.github.com>
2022-05-30 13:46:04 -0300

    up :bug:

--- Teoria/Teoria 2022/Notas.pas
+++ Teoria/Teoria 2022/Notas.pas
@@ -24,6 +24,43 @@ end;
 
 
 
+
 for i:= 1 to 1500 do
   v[i]:= abs(random(100));
+ 
+ 
+ 
+ 
+{DE LA CLESE DE TEORIA}
+
+program prueba;  //Parece que en GDB no tiene 255 caracteres.
+type
+  ascii = array[0..255] of char;
+var
+  i:integer;
+  v:ascii;
+begin
+  for i:= 0 to 255 do 
+    v[i]:= chr(i);
   
+  for i:= 0 to 255 do
+    writeln(v[i]);
+end.
+
+program prueba;  
+type
+  ascii = array[0..255] of char;
+var
+  j,i:integer;
+  v:ascii;
+begin
+  for i:= 0 to 255 do 
+    v[i]:= chr(i);
+  
+  for j:= 1 to 4 do 
+  begin
+    for i:= 1 to 10 do
+      write(v[i + 65] , ' -');
+    writeln('');
+  end;
+end.
