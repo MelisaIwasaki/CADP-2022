d0a20dbf77867b06fa78c53d5707c6cfc62d0de1
メリー緑 <92184167+MelisaIwasaki@users.noreply.github.com>
2022-06-26 14:44:29 -0300

    Create E7.pas

---
+++ Enunciado/E7.pas
@@ -0,0 +1,17 @@
+{
+Parcial 2017 15/12
+
+Un banco necesita un programa para administrar información de sus clientes. Para ello, se debe:
+a)Leer y almacenar información de los clientes. De cada cliente se lee: nro. de cliente, DNI,apellido y nombre,
+fecha de nacimiento e información de las cuentas que posee en el banco(a lo sumo 10). De cada cuenta se lee:
+CBU de la cuenta,tipo de cuenta(1: caja de ahorro,2:cuenta corriente), saldo.La lectura de cuentas,para cada 
+cliente,finaliza cuando se lee el CBU 111(que no debe procesarse).La lectura de clientes(nro.) finaliza cuando 
+se lee el código -1(que no debe procesarse).
+b)Una vez leída y almacenada toda la información, calcular e informar:
+1)Para cada cliente,el saldo total de las cajas de ahorro que posee.
+2)Nro. de los dos clientes que poseen menor cantidad de cuentas en el bco.
+3)Apellido y nombre de los clientes con DNI compuesto por más dígitos pares que impares.
+c)Implementar un módulo que adicione, en la estructura generada en “A)”, 1000 pesos al saldo de la cuenta con 
+CBU 112233445566 del cliente con nro.7777.Se asegura que existe dicho cliente y que posee dicha cuenta.
+Nota: Se pide realizar la invocación del módulo.
+}
