3860b101ad43e7c03ccd2d1a1d5b288790101905
メリー緑 <92184167+MelisaIwasaki@users.noreply.github.com>
2022-06-26 14:47:56 -0300

    Create E11.pas

---
+++ Enunciado/E11.pas
@@ -0,0 +1,12 @@
+{
+Una compañía aérea desea analizar la información de los tickets vendidos a sus clientes.La empresa cuenta con
+1200 servicios de vuelos diferentes,en adelante denominamos "vuelo".
+De cada ticket se conoce el código de vuelo(entre 1 y 1200),DNI del cliente,país de origen,país de destino,y 
+el valor de ticket.La información de los ticket se dispone en una estructura de datos adecuada, y se encuentra
+ordenada por el DNI del cliente(un cliente puede haber comprado más de un ticket en distintos vuelos).Realizar
+un programa que procese la información de los tickets y calcule:
+- Cantidad de vuelos dentro de un mismo país que hayan tenido más de 50 pasajeros.
+- Armar una estructura con los DNI y edad de los clientes que compraron 5 tickets
+- DNI del cliente que más dinero gastó en tickets.
+- Cantidad de clientes cuyo DNI terminen en 0 ó en 5.
+}
