5fa6580ed00a0698ff8197c7506e6d35e833ce2b
メリー緑 <92184167+MelisaIwasaki@users.noreply.github.com>
2022-06-26 14:48:43 -0300

    Create E12.pas

---
+++ Enunciado/E12.pas
@@ -0,0 +1,12 @@
+{
+El servicio del comercio electrónico desea analizar las ventas de notebook durante la última edición del Hot Sale.
+De cada notebook vendida se conoce:código de cliente,código de modelo(entre 1 y 200),marca y el medio de pago
+utilizado (1:Crédito;2:Débito;3:Transferencia;4:Mercado Pago;5:Bitcoin;6:Otro).Además el servicio dispone de una 
+tabla con el valor de venta de cada uno de los modelos de notebooks.
+a)Realizar un módulo que retorne la información de las ventas de notebooks en una estructura de datos adecuada.
+La información se ingresa por teclado y debe almacenarse en el orden en que fué leída.La lectura finaliza cuando
+se ingresa el código del cliente -1.
+b)Realizar un módulo que reciba la información obtenida en el inciso anterior y retorne:
+i)Los dos medios de pagos que generaron el mayor monto total en ventas.
+ii)Cantidad de notebooks de marca "Lenovo"vendidas a clientes cuyo código de cliente terminada con un dígito par. 
+}
