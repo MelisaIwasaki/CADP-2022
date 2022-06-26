21cbe1cae4731a00701a96b3b1c55c7a6ed72f4a
メリー緑 <92184167+MelisaIwasaki@users.noreply.github.com>
2022-06-26 14:50:57 -0300

    Create E15.pas

---
+++ Enunciado/E15.pas
@@ -0,0 +1,13 @@
+{
+La FIFA está analizando la información de las entradas vendidas para los 52 partidos del Mundial de fútbol Italia
+90.Para ello,dispone de una estructura de datos con información de todas las entradas vendidas.De cada entrada se 
+conoce el número de partido(entre 1 y 52)y el país de origen del comprador.La información se encuentra ordenada
+por el país de origen del comprador.
+Realizar un programa que:
+1)Invoque a un módulo que lea la información de los 52 partidos y la almacene en una estructura adecuada.De cada
+partido se conoce su número(entre 1 y 52),nombre del estadio donde se jugó y nombre de los dos equipos que 
+participaron.Esta información se ingresa por teclado y se ingresa sin ningun orden en particular.
+2)Invoque a un módulo que reciba la información de las entradas y la información de los partidos e informe:
+a)Cantidad de partidos con más de 30000 entradas vendidas.
+b)Nombre del país con mayor cantidad de entradas adquiridas para partidos en los que no juega su equipo.
+}
