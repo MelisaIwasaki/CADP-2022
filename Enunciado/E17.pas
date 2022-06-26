53e810b9978f36194e4e42673637ea62f218d20c
メリー緑 <92184167+MelisaIwasaki@users.noreply.github.com>
2022-06-26 14:52:49 -0300

    Create E17.pas

---
+++ Enunciado/E17.pas
@@ -0,0 +1,16 @@
+{
+Parcial 2021 Segunda Fecha
+
+Un teatro dispone de una estructura de datos con información de todos los tickets vendidos durante los años 1990 
+a 1999 para las 420 obras de teatro realizadas entre dichos años.De cada tickets se conoce el DNI del espectador,
+el número de asiento, el nombre de la sala, el código de obra(entre 1 y 420) y el valor del ticket. La 
+información se encuentra ordenada por código de obra.
+a)Realizar un módulo que retorne en una estructura de datos la información de las obras de teatro.De cada obra 
+se conoce su código(entre 1 y 420), título, duración en minutos y nombre del director.La información se ingresa 
+por teclado sin ningún orden en particular.
+b)Realizar un módulo que reciba la información de todos los tickets vendidos, la información de las obras y el 
+código de una obra(que podría no existir),y retorne una estructura de datos con título de la obra, duración,
+monto total de todos los tickets vendidos, y asiento, sala y valor de cada ticket vendido para dicha obra.
+Esta información debe estar ordenada por el valor de cada ticket. 
+Nota:Incluir el P.P. donde se invoquen los módulos implementados.
+}
