ab5e72a6e7cf7c789e04eebbfedf33ded817003d
メリー緑 <92184167+MelisaIwasaki@users.noreply.github.com>
2022-06-26 14:50:09 -0300

    Create E14.pas

---
+++ Enunciado/E14.pas
@@ -0,0 +1,15 @@
+{
+El comité organizador de torneo de fútbol Copa América 2021,está analizando la información de las entradas 
+vendidas para los 38 partidos del torneo.Para ello,dispone de una estructura de datos con información de 
+todas las ventas de entradas.De cada venta se conoce el código del partido(entre 1 y 38),código de cliente 
+y la cantidad de entradas vendidas.Esta información no se encuentra ordenada por ningún criterio.
+Realizar un programa que:
+a)Invoque a un módulo que lea la información de los 38 partidos, y la almacene en una estructura de datos 
+adecuada.De cada partido se conoce su código(entre 1 y 38),nombre del estadio donde se juega, la capacidad
+máxima del estadio y la hora de inicio.Esta información se ingresa por teclado y se ingresa sin ningun 
+orden en particular.
+b)Invoque a un módulo que reciba la información de las ventas de entradas y la información de los partidos 
+y retorne:
+i)Una lista con los códigos de partidos que superan el 50% de la capacidad del estadio.
+ii)Cantidad de ventas de menos de 5 entradas cuyo código de cliente termina entre 30 y 39.
+}
