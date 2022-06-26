5cc4fbba90579a0b690da8c6a68e699c30efc4b7
メリー緑 <92184167+MelisaIwasaki@users.noreply.github.com>
2022-06-26 14:55:14 -0300

    Create E20.pas

---
+++ Enunciado/E20.pas
@@ -0,0 +1,14 @@
+{
+Parcial 2021 Primera Fecha
+
+La Secretaría de apoyo a Empresas desea analizar la información de los créditos otorgados a las 1450 empresas 
+registradas en su sistema(una empresa puede tener más de un crédito).La secretaría otorga seis tipo de créditos 
+distintos: 1-Contratación del personal, 2- Compra de equipos,3-Cursos y capacitaciones,4-Asesoramiento,5-Viajes 
+y 6-Otros.Además,la secretaría dispone de una estructura de datos con información de las 1450 empresas.De cada 
+empresa se conoce su código(entre 1 y 1450), nombre,localidad y cantidad de empleados.
+a)Realice un módulo que retorne una estructura de datos con información de los créditos registrados.De cada 
+crédito se conoce el tipo de crédito, el código de la empresa(entre 1 y 1450),el monto de dinero otorgado y el 
+año en que fué otorgado.La lectura finaliza al ingresar un crédito de monto -1.
+b)Realice un módulo que reciba la información de las empresas y la información de los créditos, y retorne la 
+cantidad de créditos otorgado a empresas al menos de 40 empleados de las localidades de Neuquén o de Bariloche,
+y el tipo de crédito con menor monto total de créditos otorgados.}
