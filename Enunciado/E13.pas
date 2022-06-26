4698f2558e74b21b945be4483587f29727333886
メリー緑 <92184167+MelisaIwasaki@users.noreply.github.com>
2022-06-26 14:49:28 -0300

    Create E13.pas

---
+++ Enunciado/E13.pas
@@ -0,0 +1,12 @@
+{
+La FIFA está analizando la información de resultados en los 18 partidos del Mundial de fútbol Uruguay 30.
+Para ello,dispone de una estructura de datos con información de los goles realizados en todos los partidos.
+De cada gol se conoce el número del partido(entre 1 y 18) y el equipo que lo realizó.La información se 
+encuentra ordenada por equipo.Realizar un programa que:
+1- Invoque a un módulo que retorne información de los 18 partidos almacenada en una estructura adecuada.
+De cada partido se conoce su número(entre 1 y 18),nombre del estadio donde se jugó y nombre de los equipos 
+o paises que participaron.Esta información se ingresa por teclado, y se ingresa sin ningun orden en particular.
+2- Invoque a un módulo que reciba la información de los goles y la información de los partidos e informe:
+a)Cantidad de partidos con menos de 5 goles.
+b)Nombre del equipo con mayor cantidad de goles realizados en el Estadio Centenario.
+}
