8d18dc205cb6091e25713a459b26d765a30ece24
メリー緑 <92184167+MelisaIwasaki@users.noreply.github.com>
2022-06-26 14:54:26 -0300

    Create E19.pas

---
+++ Enunciado/E19.pas
@@ -0,0 +1,17 @@
+{
+Parcial 2021 Segunda Fecha
+
+Una empresa farmacéutica dispone de una estructura de datos con información de todos los medicamentos enviados 
+durante los años 2015 y 2016 a sus 210 distribuidoras.De cada medicamento se conoce su nombre,el mes de envío
+(1 a 12), el año(2015 a 2016), el código de la distribuidora y el valor de envío.La información se encuentra 
+ordenada por código de la distribuidora.
+a)Realizar un módulo que retorne en una estructura de datos la información de las distribuidoras.De cada 
+distribuidora se conoce su código(entre 1 y 210), nombre de la distribuidora, CUIT y provincia.La información 
+se ingresa por teclado sin ningún orden en particular.
+b)Realizar un módulo que reciba la información de los medicamentos enviados, la información de las 
+distribuidoras y el código de una distribuidora(que podría no existir)y retorne una estructura de datos con
+nombre de la distribuidora,CUIT,cantidad total de medicamentos enviados, y mes, año y valor de cada medicamento
+enviado a dicha distribuidora.Esta información debe estar ordenada por el valor de cada medicamento.
+Nota:Incluir el P.P. donde se invoquen los módulos implementados.
+
+}
