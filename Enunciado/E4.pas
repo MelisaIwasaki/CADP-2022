1c6ef39d95436061e5fe74d6da0e86f7ba8cc18b
メリー緑 <92184167+MelisaIwasaki@users.noreply.github.com>
2022-06-26 14:42:03 -0300

    Create E4.pas

---
+++ Enunciado/E4.pas
@@ -0,0 +1,19 @@
+{
+Parcial 2016 16/12
+
+Un comercio de la ciudad de La Plata necesita un programa para administrar la información de las ventas 
+realizadas durante Noviembre de 2016. El comercio dispone de información sobre los 1000 productos que 
+comercializa. De cada producto se conoce: descripción y precio unitario.
+Se debe: 
+a)Leer y almacenar la información de las ventas. De cada venta se lee: código de venta, DNI del comprador, 
+código del producto comprado(1 .. 1000) y cantidades de unidades de producto adquiridas. La lectura de 
+información finaliza cuando se lee la venta con código de venta 2121, la cual debe procesarse.
+b)Una vez leída y almacenada toda la información, calcular e informar:
+1)Código de venta y monto total de cada venta.
+2)Código y descripción del producto más vendido y del producto menos vendido.
+3)Cantidad de ventas con DNI del comprador compuesto por al menos tres dígitos pares.
+c)Realizar un módulo que aumente, en la estructura que se dispone, un 15% el precio unitario del producto 
+cuya descripción se recibe como parámetro (dicha descripción puede no existir).Tener en cuenta que no se 
+repiten las descripciones de los productos en la estructura que se dispone. 
+
+}
