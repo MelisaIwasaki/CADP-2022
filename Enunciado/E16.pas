62e25a0ad8ac523055c68afdaa5f35535939ef65
メリー緑 <92184167+MelisaIwasaki@users.noreply.github.com>
2022-06-26 14:52:02 -0300

    Create E16.pas

---
+++ Enunciado/E16.pas
@@ -0,0 +1,15 @@
+{
+Parcial 2021 Tercera Fecha 
+
+El Sindicato de trabajadores de juego de azar está analizando la información de los ganadores de loterías y quinielas. El sindicato dispone de una estructura de datos 
+con información de todos los premios ganados. De cada premio ganado se conoce: DNI del ganador, su edad en años, número ganador, nombre del juego(quiniela matutina,
+quiniela vespertina,etc.) y el monto del premio obtenido. La información se encuentra ordenada por el nombre del juego.
+a)Realice un módulo que reciba la información de todos los ganadores y una edad, y retorne la información de todos los ganadores de premios de menos de 100.000 pesos 
+mayores de dicha edad.
+b)Realice un módulo que reciba la información de todos los ganadores de premios de menos de 100.000 pesos mayores de 20 años y, a partir de la misma, informe:
+-Monto total obtenido por cada juego.
+-Cantidad de ocurrencias de cada dígito( del 0 al 9) entre los números ganadores.
+-DNI del ganador que obtuvo el menor premio.
+Nota:Incluir el P.P. donde se invoquen los módulos implementados.
+
+}
