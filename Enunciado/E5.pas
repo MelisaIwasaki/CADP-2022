4dd78d9b3cbeee03803d1272b5502f37c5ae283b
メリー緑 <92184167+MelisaIwasaki@users.noreply.github.com>
2022-06-26 14:42:54 -0300

    Create E5.pas

---
+++ Enunciado/E5.pas
@@ -0,0 +1,18 @@
+{
+Parcial 2017 25/11
+
+Una biblioteca necesita un programa para administrar información de los préstamos de libros realizados, a sus 
+socios, durante 2017. Para ello, se debe:
+a)Leer y almacenar información de los préstamos. De cada préstamo se lee: número de préstamo, fecha de préstamo
+( día, mes) , número de socio, apellido y nombre del socio, ISBN de libro, título del libro. La lectura de 
+información finaliza cuando se lee el ISBN de libro -1(que no debe procesarse). La información debe ser 
+almacenada de manera ordenada por ISBN de libro.Tener en cuenta que el libro puede haber sido prestado 0, 1 o 
+más veces.
+b)Una vez leída y almacenada toda la información, calcular e informar:
+1) Para cada libro, su título y la cantidad de veces que fue prestado en el mes de Marzo.
+2) Apellido y nombre de los socios con número de socio compuesto sólo por dígitos múltiplos de 3.
+3)Mes en el que se realizó la mayor cantidad de préstamos y mes en el que se realizó la menor cantidad
+C)  Implementar un módulo que actualice, en la estructura generada en “A)”, el título del    libro con 
+ISBN 1122334455, el nuevo título debería ser “Introducción a la Programación”.
+Nota: Se pide realizar la invocación del módulo.
+}
