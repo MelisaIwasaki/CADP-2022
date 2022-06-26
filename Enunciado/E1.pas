2b51ae9754f7c6ec4ce1c4403cc60e484bc8d224
メリー緑 <92184167+MelisaIwasaki@users.noreply.github.com>
2022-06-26 14:39:40 -0300

    Create E1.pas

---
+++ Enunciado/E1.pas
@@ -0,0 +1,16 @@
+{
+Parcial 2015 18/7
+
+Una consultora informática necesita organizar la información de sus proyectos. Para ello se debe: Leer y 
+almacenar la información de los proyectos. De cada proyecto se lee: número interno del proyecto, descripción 
+del proyecto, año de inicio, año de fin, código del tipo del lenguaje utilizado (1- PHP, 2- Java, 3- Phyton, 
+4- Net, 5- Ruby) el costo de la hora de programación.La lectura finaliza con el número interno 0.
+Una vez leída y almacenada la información de los proyectos se pide:
+a- Calcular e informar los dos lenguajes de programación más utilizados.
+b- Calcular e informar para cada año entre 2000 y 2014 el costo total de horas de programación de los proyectos 
+cuyo “año de inicio” se encuentre en dicho rango (sin importar el año del fin).
+c- Calcular e informar la cantidad de personas que participaron en proyectos en los que se utilizó el lenguaje 
+“Phyton” y con duración menor a un año.
+d- Calcular e informar la cantidad de proyectos en los que el número interno contenga a lo sumo 3 dígitos 
+impares.
+}
