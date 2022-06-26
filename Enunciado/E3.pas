91bc95f4c60750d0f1132f77a450b3bf7897509d
メリー緑 <92184167+MelisaIwasaki@users.noreply.github.com>
2022-06-26 14:41:10 -0300

    Create E3.pas

---
+++ Enunciado/E3.pas
@@ -0,0 +1,16 @@
+{
+Parcial 2015 14/11
+
+Desarrollar un programa, destinado a la gestión de inscripciones de alumnos de la Facultad de Informática a una
+capacitación sobre aplicaciones para Smartphones. Para ello, se debe:
+a)Leer y almacenar la información de los alumnos interesados. De cada alumno se lee y almacena: DNI, apellido, 
+nombre, año de ingreso, código de la carrera en la que se encuentra inscripto(1- APU, 2- LS, 3- LI, 4- IC) y 
+analítico. El analítico contiene el nombre y la nota de las materias aprobadas, a lo sumo 32. La lectura de los 
+alumnos interesados finaliza cuando llega el DNI 0 (cero), y la lectura de las materias para cada alumno 
+finaliza cuando se lee el nombre de la materia “ZZZ”.
+Una vez leída y almacenada la información, se pide:
+b)Informar los dos códigos de carrera con menos alumnos inscriptos.
+c)Informar el DNI,nombre y apellido de los alumnos cuyo DNI poseen a lo sumo 3 dígitos impares.
+d)Informar el nombre, apellido y año de ingreso del alumno inscripto con mejor promedio entre todas las carreras.
+Nota: Un alumno se encuentra inscripto a una única carrera.
+}
