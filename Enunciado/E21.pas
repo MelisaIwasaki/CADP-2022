e527a928595253d5bc4bbf0f8e34ad278e9e26d0
メリー緑 <92184167+MelisaIwasaki@users.noreply.github.com>
2022-06-26 14:56:02 -0300

    Create E21.pas

---
+++ Enunciado/E21.pas
@@ -0,0 +1,16 @@
+{
+Parcial 2021 Primera Fecha
+
+Un local de ventas de dispositivos de almacenamiento está analizando las ventas realizadas durante la navidad 
+de 2020.El local organiza los dispositivos que vende en 5 categorías:1-Discos Magnéticos Externos,2-Discos 
+Sólidos Externos,3-Pendrives,4-Tarjeta de memoria,5-Otros.Además,el local dispone de una estructura de datos 
+con información de todas sus ventas.De cada venta se conoce el código de dispositivo vendido, la cantidad 
+vendida y el DNI del cliente.
+a)Realice un módulo que retorne una estructura de datos con la información de los 650 dispositivos que tiene a 
+la venta.De cada dispositivo conece su categoría(1 a 5),cantidad de gigabytes(GB) de almacenamiento, marca,
+precio y código (entre 1 y 650).La información de cada dispositivo es ingresada por teclado sin ningún orden 
+en particular.
+b)Realice un módulo que reciba la información de la venta y la información de los dispositivos, y retorne la 
+cantidad de ventas de dispositivos de marca Kingston de a lo sumo de 200GB de cualquier categoría, y la 
+categoría de dispositivos con menor monto total de ventas.
+}
