df64690c942bbc837c965a14224afd7448fa86b4
メリー緑 <92184167+MelisaIwasaki@users.noreply.github.com>
2022-06-26 14:40:26 -0300

    Create E2.pas

---
+++ Enunciado/E2.pas
@@ -0,0 +1,19 @@
+{
+Parcial 2015 13/6
+ 
+Se va a desarrollar un concurso de docentes universitarios que se dedican a la investigación y se debe realizar 
+un programa para administrar dicho concurso. Para ello se lee desde teclado información de docentes. De cada 
+docente se lee: DNI, apellido, nombre, código de la facultad a la que pertenece(1 .. 17), código de categoría de 
+docente(1 .. 5), área de investigación y cantidad de años en investigación. La lectura finaliza cuando llega el 
+docente con DNI 0.
+Además, para desarrollar el programa se dispone de una tabla que indica para cada categoría de docente el 
+puntaje básico que se otorga por año de investigación.
+Una vez que ha leída y almacenada la información de los docentes, se pide:
+a)Informar para cada docente el DNI, apellido, nombre y puntaje total otorgado según sus años de investigación 
+y su categoría de docente. El puntaje total se calcula como: año de investigación del docente multiplicado por 
+el valor que indique la tabla para su categoría de docente.
+b)Calcular e informar el código de facultad con mayor cantidad de docentes universitarios categoría 2.
+c)Informar cuántos docentes categoría 5 se desempeñan en el área de “Ingeniería de software”.
+d)Informar el DNI, apellido y nombre de aquellos docentes cuyo DNI contenga igual cantidad de dígitos pares que 
+impares.
+}
