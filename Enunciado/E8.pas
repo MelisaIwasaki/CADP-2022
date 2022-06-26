4c69e4d7773adc2408e33c34c626702c03855a8d
メリー緑 <92184167+MelisaIwasaki@users.noreply.github.com>
2022-06-26 14:45:20 -0300

    Create E8.pas

---
+++ Enunciado/E8.pas
@@ -0,0 +1,14 @@
+{
+La plataforma YouTube está analizando algunos de sus principales canales de video.Para eso YouTube cuenta con
+una tablacon el nombre de las 60 categorías que utilizan para clasificar los videos(música,educación y ciencia,
+hogar,deportes,etc).De cada video se conoce su título,nombre del canal donde fué publicado, duración en segundos,
+año de publicación,cantidades de visualizaciones y el código de categoría a la que pertenece el video(1..60).
+Realizar un programa que:
+A)Invoque a un módulo.Que lea la información de los videos hasta ingresar un video de duración -1(que no debe 
+procesarse)y devuelva en una estructura de datos adecuada para el canal "Paulina Cocina",la siguiente información:
+-Nombre de canal de videos publicados.
+-Cantidad total de visualizaciones entre todos los videos.
+-Detalle del título,nombre de la categoría y duración de todos los videos con más de 1000 visualizaciones.
+B)Invoque a un módulo que reciba a estructura generada en A) e imprima nombre del canal y el título de cada 
+video con más de 1000 visualizaciones.
+}
