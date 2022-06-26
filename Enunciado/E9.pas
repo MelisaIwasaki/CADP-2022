20e56919831ff062f9f62b36157e644ed4effc08
メリー緑 <92184167+MelisaIwasaki@users.noreply.github.com>
2022-06-26 14:46:01 -0300

    Create E9.pas

---
+++ Enunciado/E9.pas
@@ -0,0 +1,11 @@
+{
+Una consesionaria de la ciudad de La Plata necesita un programa para administrar información de ventas de autos 
+efectuadas en Febrero de 2020.Para ello se debe leer la información de ventas realizadas.De cada venta se lee
+su código de venta, modelo de auto,DNI del cliente al que se vendió el auto,el día de la venta(1..28).La 
+información de la venta se lee de manera ordenada por modelo de auto y finaliza cuando se ingresa el modelo
+'ZZZ'(que no debe procesarse).Se pide:
+- Generar una estructura que contenga,para cada modelo de auto la cantidad de veces que fué vendido.Esta 
+estructura debe quedar ordenada por modelo de auto.
+- Calcular e informar el día del mes en que se realizaron más ventas a cliente con DNI impar.
+- Calcular e informar el porcentaje de ventas con código par.
+}
