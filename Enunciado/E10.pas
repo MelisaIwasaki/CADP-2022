87c50d4921e387a1ece01a2ce6ce6a9fd0ee9699
メリー緑 <92184167+MelisaIwasaki@users.noreply.github.com>
2022-06-26 14:46:54 -0300

    Create E10.pas

---
+++ Enunciado/E10.pas
@@ -0,0 +1,11 @@
+{
+Un centro de estética ofrece 12 planes para sus clientes:ozonoterepia,cama solar,spa,depilación,tratamientos 
+antiarrugas,etc.Cada plan posee un código(1..12),un costo y una cantidad de sesiones.Se dispone de una estructura
+con esta información.El centro de estética posee como máximo 90000 clientes.Realizar un programa que lea la 
+información de clientes del centro de estética.De cada cliente se lee:nombre,DNI,ciudad de origen,código de plan
+contratado,y si ya finalizó el tratamiento.La información de los clientes finaliza al ingresar al cliente
+'Mirtha Legrand'con DNI 2320718(que debe procesarse)o al alcanzarse el máximo de clientes.Una vez leída y 
+almacenada la información,procesar los datos para:
+1)Informar la ganancia del centro para cada tipo de plan(incluyendo tratamientos finalizados y no finalizados).
+2)Almacenar en una lista todos los datos de los clientes cuyo tratamientos aún no han finalizados.
+}
