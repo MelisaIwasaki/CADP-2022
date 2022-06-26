05d67d22623eeeecf675913b7f0cf35c0cdec881
メリー緑 <92184167+MelisaIwasaki@users.noreply.github.com>
2022-06-26 14:53:41 -0300

    Create E18.pas

---
+++ Enunciado/E18.pas
@@ -0,0 +1,16 @@
+{
+Parcial 2021 Segunda Fecha
+
+Una aerolínea dispone de una estructura de datos con información de todos los pasajes vendidos durante los años 
+2003 a 2014 por sus 6700 agencias oficiales. De cada pasaje se conoce el país de destino, el mes de la 
+venta(1 a 12),el año,el número de agencia (entre 1 y 6700) y el costo de pasaje. La información se encuentra 
+ordenada por número de agencia.
+a)Realizar un módulo que retorne en una estructura de datos la información de las agencias.De cada agencia se 
+conoce su código(entre 1 y 6700),razón social y localidad.La información se ingresa por teclado sin ningún 
+orden en particular.
+b)Realizar un módulo que reciba la información de los pasajes vendidos, la información de las  agencias oficiales
+y el código de una agencia(que podría no existir), y retorne una estructura de datos con nombre de la agencia,
+razón social, localidad, monto total vendido, y mes,año y valor de cada pasaje vendido por dicha agencia.
+Esta información debe estar ordenada por el valor de cada pasaje.
+Nota:Incluir el P.P. donde se invoquen los módulos implementados.
+}
